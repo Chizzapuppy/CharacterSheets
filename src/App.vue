e79936e94cf2350e7fbaf881aedb2e2e47e51c53
Rachel <rkirkland08@yahoo.com>
2020-10-04 18:29:47 -0400

    added Keith

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <div><mattis></mattis></div>
+  <div><keith></keith></div>
 </template>
 
 <script>
-import Mattis from './components/Mattis/main.vue'
+//import Mattis from './components/Mattis/main.vue'
+import Keith from './components/Keith/main.vue'
 
 export default {
   name: 'App',
   components: {
-    Mattis
+    //Mattis,
+    Keith
   }
 }
 </script>
